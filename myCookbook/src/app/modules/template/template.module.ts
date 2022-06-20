9965143850c9d4d3afb9c3223ae49f591af1bcda
Filip Ryjáček (ext) <filip.ryjacek@ext.csas.cz>
2022-06-20 21:19:22 +0200

    forms: template

--- myCookbook/src/app/modules/template/template.module.ts
+++ myCookbook/src/app/modules/template/template.module.ts
@@ -6,12 +6,14 @@ import { TemplateComponent } from './template.component';
 import { RorwformComponent } from './rorwform/rorwform.component';
 import {AccordionModule} from 'primeng/accordion';
 import { RoComponent } from './ro/ro.component';
+import { Form1Component } from './form1/form1.component';
 
 @NgModule({
   declarations: [
     TemplateComponent,
     RorwformComponent,
-    RoComponent
+    RoComponent,
+    Form1Component
   ],
   imports: [
     CommonModule,
